57b1e6e7349cb3f2cf3a9416981dd66ab01ffc12
Michael Lehenauer <michael@lehenauer.com>
2025-03-10 10:22:02 +0100

    wezterm dings

--- .wezterm.lua
+++ .wezterm.lua
@@ -36,27 +36,6 @@ local function scheme_for_appearance(appearance)
 end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
-config.window_frame = {
-	-- The font used in the tab bar.
-	-- Roboto Bold is the default; this font is bundled
-	-- with wezterm.
-	-- Whatever font is selected here, it will have the
-	-- main font setting appended to it to pick up any
-	-- fallback fonts you may have used there.
-	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
-
-	-- The size of the font in the tab bar.
-	-- Default to 10.0 on Windows but 12.0 on other systems
-	font_size = 14.0,
-
-	-- The overall background color of the tab bar when
-	-- the window is focused
-	active_titlebar_bg = "#1E1E2E",
-
-	-- The overall background color of the tab bar when
-	-- the window is not focused
-	inactive_titlebar_bg = "#1E1E2E",
-}
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
 	left = 5,
