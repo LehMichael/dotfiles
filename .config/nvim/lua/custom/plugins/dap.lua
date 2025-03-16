5a649b1b8c1494c6a683a7ac8db87bc49ae2c895
Michael Lehenauer <michael@lehenauer.com>
2025-03-16 22:37:27 +0100

    remove notifications and add check for node debug adapter

--- .config/nvim/lua/custom/plugins/dap.lua
+++ .config/nvim/lua/custom/plugins/dap.lua
@@ -1,56 +1,3 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
---
--- Utility functions shared between progress reports for LSP and DAP
-
-local client_notifs = {}
-
-local function get_notif_data(client_id, token)
-    if not client_notifs[client_id] then
-        client_notifs[client_id] = {}
-    end
-
-    if not client_notifs[client_id][token] then
-        client_notifs[client_id][token] = {}
-    end
-
-    return client_notifs[client_id][token]
-end
-
-local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
-
-local function update_spinner(client_id, token)
-    local notif_data = get_notif_data(client_id, token)
-
-    if notif_data.spinner then
-        local new_spinner = (notif_data.spinner + 1) % #spinner_frames
-        notif_data.spinner = new_spinner
-
-        notif_data.notification = vim.notify(nil, nil, {
-            hide_from_history = true,
-            icon = spinner_frames[new_spinner],
-            replace = notif_data.notification,
-        })
-
-        vim.defer_fn(function()
-            update_spinner(client_id, token)
-        end, 100)
-    end
-end
-
-local function format_title(title, client_name)
-    return client_name .. (#title > 0 and ": " .. title or "")
-end
-
-local function format_message(message, percentage)
-    return (percentage and percentage .. "%\t" or "") .. (message or "")
-end
-
 return {
     "mfussenegger/nvim-dap",
     dependencies = {
@@ -199,17 +146,20 @@ return {
 
         local jspath
         local ok = pcall(function()
-            jspath = require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+            jspath = vim.fs.joinpath(
+                require("mason-registry").get_package("js-debug-adapter"):get_install_path(),
+                "/js-debug/src/dapDebugServer.js"
+            )
         end)
 
-        if ok then
+        if ok and vim.fn.filereadable(jspath) then
             local node_adapter = {
                 type = "server",
                 host = "localhost",
                 port = "${port}",
                 executable = {
                     command = "node",
-                    args = { jspath .. "/js-debug/src/dapDebugServer.js", "${port}" },
+                    args = { jspath, "${port}" },
                 },
                 enrich_config = function(conf, on_config)
                     if not vim.startswith(conf.type, "pwa-") then
@@ -244,50 +194,5 @@ return {
                 detached = vim.fn.has("win32") == 0,
             },
         })
-        -- DAP integration
-        -- Make sure to also have the snippet with the common helper functions in your config!
-
-        dap.listeners.before["event_progressStart"]["progress-notifications"] = function(
-            session,
-            body
-        )
-            local notif_data = get_notif_data("dap", body.progressId)
-
-            local message = format_message(body.message, body.percentage)
-            notif_data.notification = vim.notify(message, "info", {
-                title = format_title(body.title, session.config.type),
-                icon = spinner_frames[1],
-                timeout = false,
-                hide_from_history = false,
-            })
-
-            notif_data.notification.spinner = 1, update_spinner("dap", body.progressId)
-        end
-
-        dap.listeners.before["event_progressUpdate"]["progress-notifications"] = function(
-            session,
-            body
-        )
-            local notif_data = get_notif_data("dap", body.progressId)
-            notif_data.notification =
-                vim.notify(format_message(body.message, body.percentage), "info", {
-                    replace = notif_data.notification,
-                    hide_from_history = false,
-                })
-        end
-
-        dap.listeners.before["event_progressEnd"]["progress-notifications"] = function(
-            session,
-            body
-        )
-            local notif_data = client_notifs["dap"][body.progressId]
-            notif_data.notification =
-                vim.notify(body.message and format_message(body.message) or "Complete", "info", {
-                    icon = "",
-                    replace = notif_data.notification,
-                    timeout = 3000,
-                })
-            notif_data.spinner = nil
-        end
     end,
 }
