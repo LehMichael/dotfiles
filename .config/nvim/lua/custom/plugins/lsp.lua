2cca3fa87103d3caf264e12f5ec5e0c42dc95f36
Michael Lehenauer <michael@lehenauer.com>
2026-03-09 01:14:56 +0100

    [nvim/lsp] change inlay hint check bc it was apparently deprecated

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -22,10 +22,7 @@ return {
                 end
 
                 local client = vim.lsp.get_client_by_id(event.data.client_id)
-                if
-                    client
-                    and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint)
-                then
+                if client and client:supports_method("textDocument/inlayHint", event.buf) then
                     map("<leader>th", function()
                         vim.lsp.inlay_hint.enable(
                             not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })
