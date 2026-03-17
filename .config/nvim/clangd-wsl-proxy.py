1016485a457661024343a67a03c68b7b08bf213a
Michael Lehenauer <michael@lehenauer.com>
2026-03-17 20:54:12 +0100

    [nvim/lsp] add shim for clangd in wls

---
+++ .config/nvim/clangd-wsl-proxy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import subprocess, sys, os, threading, re
+
+CLANGD = r"/mnt/c/Program Files/Microsoft Visual Studio/18/Community/VC/Tools/Llvm/x64/bin/clangd.exe"
+WSL   = "file:///mnt/c/"
+WIN   = "file:///C:/"
+
+def translate(data: bytes, src: str, dst: str) -> bytes:
+    return data.replace(src.encode(), dst.encode())
+
+def read_message(stream) -> bytes | None:
+    header = b""
+    while not header.endswith(b"\r\n\r\n"):
+        ch = stream.read(1)
+        if not ch:
+            return None
+        header += ch
+    length = int(re.search(rb"Content-Length: (\d+)", header).group(1))
+    return header + stream.read(length)
+
+def pipe(src, dst, wsl_to_win: bool):
+    while True:
+        msg = read_message(src)
+        if msg is None:
+            break
+        if wsl_to_win:
+            msg = translate(msg, WSL, WIN)
+        else:
+            msg = translate(msg, WIN, WSL)
+        # fix Content-Length after translation (length may have changed)
+        header, body = msg.split(b"\r\n\r\n", 1)
+        header = re.sub(rb"Content-Length: \d+",
+                        f"Content-Length: {len(body)}".encode(), header)
+        dst.write(header + b"\r\n\r\n" + body)
+        dst.flush()
+
+args = [CLANGD] + sys.argv[1:]
+proc = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+t = threading.Thread(target=pipe, args=(sys.stdin.buffer, proc.stdin, True))
+t.daemon = True
+t.start()
+
+pipe(proc.stdout, sys.stdout.buffer, False)
