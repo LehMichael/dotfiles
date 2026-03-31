51268f6da622e5a967eaf17f9663f926f2aab86f
Michael Lehenauer <michael@lehenauer.com>
2026-03-31 11:30:04 +0200

    [nvim] remove compiler nvim

--- .config/nvim/lua/custom/plugins/overseer.lua
+++ .config/nvim/lua/custom/plugins/overseer.lua
@@ -1,10 +1,4 @@
 return {
-    { -- This plugin
-        "Zeioth/compiler.nvim",
-        cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-        -- dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
-        opts = {},
-    },
     {
         "stevearc/overseer.nvim",
         opts = {
